a65cadefff6dca408f21f2228bf89c9d978461c5
Vani Shivanand <vani.shivanand@gmail.com>
2020-02-29 03:34:44 +0530

    updating apis

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const express =  require('express');
+const express = require('express');
 
 const app = express();
 // The Firebase Admin SDK to access the Firebase Realtime Database.
@@ -8,7 +8,7 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 
 // Your web app's Firebase configuration
-  var firebaseConfig = {
+var firebaseConfig = {
     // apiKey: "AIzaSyDsIqphipO5CrmnTRpBMdNz35fuDb4yzfA",
     // authDomain: "gaadicalls.firebaseapp.com",
     databaseURL: "https://gaadicalls.firebaseio.com",
@@ -16,23 +16,22 @@ const serviceAccount = require("./serviceAccountKey.json");
     // storageBucket: "gaadicalls.appspot.com",
     // messagingSenderId: "142609654495",
     // appId: "1:142609654495:web:55c9d4dd125349355dff6a",
-    credential: admin.credential.cert(serviceAccount),
-    // databaseURL: 'https://<PROJECT-ID >.firebaseio.com'
-  };
-  // Initialize Firebase
-  admin.initializeApp(firebaseConfig);
+    credential: admin.credential.cert(serviceAccount)
+};
+// Initialize Firebase
+admin.initializeApp(firebaseConfig);
 
-  const db = admin.firestore();
+const db = admin.firestore();
 
 app.get('/track', function (req, res) {
     res.send({
-        lat : 33,
-        lng : 33
+        lat: 33,
+        lng: 33
     });
 });
 
-app.get('/add_user/:id/:lat/:lng',async function (req, res) {
-    const snapshot = await db.collection("Householders").add(
+app.get('/add_user/:id/:lat/:lng', async function (req, res) {
+    await db.collection("Householders").add(
         {
             "DeviceId": req.params.id,
             "lat": req.params.lat,
@@ -41,18 +40,36 @@ app.get('/add_user/:id/:lat/:lng',async function (req, res) {
     res.send('add user success');
 });
 
-app.get('/add_vendor', function (req, res) {
-    res.send('Hello World')
+app.get('/add_vendor/:id', async function (req, res) {
+    await db.collection("WalkingVendors").add(
+        {
+            "DeviceId": req.params.id
+        });
+    res.send('vendor added')
 });
 
 
 //Vendor app need to update location again and again
-app.get('/update_location', function (req, res) {
-    res.send('Hello World')
+app.get('/update_location/:id/:lat/:lng', async function (req, res) {
+
+    let listOfAll = await db.collection("Householders").getDocuments();
+    await listOfAll.documents.forEach((entry) => {
+        if (distance(req.params.lat, req.params.lng, entry.data["lat"], entry.data["lng"], "K") < 2) {
+            let payload = {
+                notification: {
+                    title: 'Vendor is nearby',
+                    body: ''
+                    }
+            };
+            admin.messaging().sendToDevice("Random"+entry.data["DeviceId"], payload);
+        }
+
+    });
+    res.send('update location of vendor success');
 });
 
 //For the householder to add location once
-app.get('/add_location', function (req, res) {
+app.get('/associate_vendor/:id/:vid', function (req, res) {
     res.send('Hello World')
 });
 
@@ -85,25 +102,25 @@ app.get('/add_location', function (req, res) {
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 function distance(lat1, lon1, lat2, lon2, unit) {
-	if ((lat1 == lat2) && (lon1 == lon2)) {
-		return 0;
-	}
-	else {
-		var radlat1 = Math.PI * lat1/180;
-		var radlat2 = Math.PI * lat2/180;
-		var theta = lon1-lon2;
-		var radtheta = Math.PI * theta/180;
-		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		if (dist > 1) {
-			dist = 1;
-		}
-		dist = Math.acos(dist);
-		dist = dist * 180/Math.PI;
-		dist = dist * 60 * 1.1515;
-		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
-		return dist;
-	}
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1 / 180;
+        var radlat2 = Math.PI * lat2 / 180;
+        var theta = lon1 - lon2;
+        var radtheta = Math.PI * theta / 180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") { dist = dist * 1.609344 }
+        if (unit == "N") { dist = dist * 0.8684 }
+        return dist;
+    }
 }
 
 exports.app = functions.https.onRequest(app);
